807bd694e17df35ebc7951ecd4772e24d4dab87e
Oussama CHAIB <ousszoch@outlook.com>
2021-11-14 11:42:34 +0100

    main

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+class Contact extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			CheckExistantOwner : "Before"
+		};
+	}
+	componentDidMount() { console.log("Begin");
+		const data = { Email: "chaiboussama92@gmail.com" };
+		const headers = {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json',
+			'Authorization': 'Bearer my-token',
+			'My-Custom-Header': 'foobar'
+		}
+		axios.post("http://localhost:8081/session", {data}, {headers})
+		.then(res => { console.log("Yes ", res.data); })
+		.catch(error => console.log("No ", error));
+	}
+	componentWillMount() {
+		
+		
+	}
+	SendMessage() { 
+		//var EmptyField = { border : "2px ridge red" };
+		var NoEmptyField = {};
+		this.setState({ 
+			NameStyle: NoEmptyField 
+		}); 
+		
+		
+	}
+
+	render() {	
+		return (<div className="form-contact">
+					<h1>{this.state.CheckExistantOwner}</h1>
+					
+					<div className="form-field text-center">
+						<button onClick={() => this.SendMessage} className="submit button">Envoyer</button>
+					</div>
+				</div>);	
+	}
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Contact />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
